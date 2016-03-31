c958d2b8c7868b09fde96a5f2d8a4aa270cb108c
Lucas Mosele <lucasmosele@gmail.com>
2016-03-30 21:35:13 -0400

    init commit

---
+++ test.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:fb="http://ogp.me/ns/fb#">
+<head>
+	<title>Maxwell Health | 2015 End of Year Report</title>
+	<meta charset="utf-8" />
+    <meta property="og:image" content="./dist/img/ui/fb_preview.jpg" />
+    <meta name="description" content="Here at Maxwell, we made exciting strides in 2015 on our journey to help American families achieve health and wealth success through a simple and affordable subscription service. Join us as we take a literal walk down memory lane and highlight some milestones from the past year.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="./dist/css/main.css">
+</head>
+<body>
+	<!-- SKY -->
+	<div id="sky" 
+		data-0="background:linear-gradient(    -90deg, rgb(169,217,239) 60%, rgb(227,220,202)   );" 
+		data-600="background:linear-gradient( -90deg, rgb(169,217,239) 10%, rgb(250,250,250) );"
+		data-2500="background:linear-gradient( -90deg, rgb(137,197,233) 10%, rgb(250,250,250) );"
+		data-3500="background:linear-gradient( -90deg, rgb(111,138,196) 10%, rgb(178,90,159)  );"
+		data-5000="background:linear-gradient( -90deg, rgb(47,54,154) 5%,   rgb(151,62,96)     );"
+		data-6500="background:linear-gradient( -90deg, rgb(12,24,61) 5%,   rgb(23,71,105)     );"
+		data-8500="background:linear-gradient( -90deg, rgb(12,24,61) 10%,   rgb(23,71,105)     );">
+		<?php echo file_get_contents("./dist/img/movers/cloud1.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/cloud2.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/cloud3.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/cloud4.svg"); ?>
+		<?php echo file_get_contents("./dist/img/background/star.svg"); ?>
+		<?php echo file_get_contents("./dist/img/background/starfield.svg"); ?>
+	</div>
+
+	<div id="otherClouds" 
+	data-0="transform: translate3d(0%,20%,0);opacity:.1;" data-100="transform: translate3d(-0%,0,0);opacity:.7;" data-3500="opacity:.0;">
+		<?php echo file_get_contents("./dist/img/movers/cloud5.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/cloud6.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/cloud7.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/cloud8.svg"); ?>
+	</div>
+	
+
+	<!-- NAVBAR -->
+	<?php echo file_get_contents("eoy/src/includes/_navbar.php"); ?>
+
+	<!-- CONTENT -->
+	<div id="fullpage">
+	    <div class="section middle">
+	    	<div class="intro">
+	    		<h2>Maxwell Health 2015 Report</h2>
+	    		<p>Here at Maxwell, we made exciting strides in 2015 on our journey to help American families achieve health and wealth success through a simple and affordable subscription service. </p>
+				<p>We also made strides geographically&hellip; by moving our headquarters a few blocks down the street to 101 Tremont. </p>
+				<p>Join us as we take a literal walk down memory lane and highlight some milestones from the past year.</p>
+
+	    		<button class="btn ghost" onclick="$.fn.fullpage.moveSectionDown();">See The Results</button>
+	    	</div>
+	    </div>
+
+	    <!-- CHAP 1 -->
+	    <div class="section"> 
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>We Expanded Our Network</h2>
+	    		<p>We partnered with many of the <span class="bold">top 100 brokers</span> in the country, including:</p>
+	    		<img class="mainImage" src="./dist/img/content/logos.png">
+	    		
+	    	</div>
+	    </div>
+	    <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>We Hailed our Champions</h2>
+	    		<p>We held our first partner events in San Francisco and Boston! </p>
+	    		<img class="mainImage" src="./dist/img/content/champion.png">
+	    	</div>
+	    </div>
+	    <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>Our User Base Grew</h2>
+	    		<p>The number of employees using Maxwell to enroll in and manage their benefits grew by <span class="big"> over 200%</span></p>
+	    	</div>
+	    </div>
+
+
+	    <!-- CHAP 2 -->
+	    <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>We Solved Challenges</h2>
+	    		<p>We tackled the retirement savings crisis with MassMutual</p>
+	    		<img class="mainImage" src="./dist/img/content/beneclick.png">
+	    		<div class="press">
+	    			<a href="http://www.benefitspro.com/2015/10/07/massmutual-launches-integrated-benefit-exchange" target="_blank"><img src="./dist/img/content/tile-benefitspro.png"></a>
+	    			<a href="http://www.forbes.com/sites/davechase/2015/11/02/the-7-organizations-that-will-turn-healthcare-upside-down-in-2016/" target="_blank"><img src="./dist/img/content/tile-forbes.png"></a>
+	    			<a href="http://www.hreonline.com/HRE/view/story.jhtml?id=534359456" target="_blank"><img src="./dist/img/content/tile-executive.png"></a>
+	    		</div>
+	    	</div>
+	    </div>
+	    <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>We Grew Our Marketplace</h2>
+	    		<p>We added <span class="bold">best-in-class</span> carriers and lifestyle products to our Marketplace, including:</p>
+	    		<img class="mainImage" src="./dist/img/content/marketplace.png">
+	    	</div>
+	    </div>
+
+	    <!-- CHAP 3 -->
+	    <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>We Shared Our Knowledge</h2>
+	    		<p>We were a featured speaker at <span class="bold">11 of the top</span> industry events across the country, including:</p>
+	    		<img class="mainImage knowledge" src="./dist/img/content/knowledge.png">
+	    	</div>
+	    </div>
+	    <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>We Were Recognized</h2>
+	    		<p>...and honored.</p>
+	    		<div class="press">
+	    			<a href="http://www.bizjournals.com/boston/news/2015/10/29/meet-the-boston-business-journals-innovation-all.html#g8" target="_blank"><img src="./dist/img/content/tile-BBJ.png"></a>
+	    			<a href="http://bostinno.streetwise.co/all-series/meet-the-50-on-fire-finalists-in-healthcare-medicine/" target="_blank"><img src="./dist/img/content/tile-50onf.png"></a>
+	    			<a href="http://bostinno.streetwise.co/all-series/announcing-this-years-coolest-companies-nominees/" target="_blank"><img src="./dist/img/content/tile-bostinno.png"></a>
+	    		</div>
+	    	</div>
+	    </div>
+
+	    <!-- CHAP 5 -->
+	     <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>Our Leadship Grew</h2>
+	    		<p>We added some rock stars to our already awesome team.</p>
+	    		<div class="press">
+	    			<a href="http://info.maxwellhealth.com/blog/mike-murphy-joins-maxwell-health-as-vp-of-sales" target="_blank"><img src="./dist/img/content/tile-murphy.png"></a>
+	    			<a href="http://info.maxwellhealth.com/blog/welcome-to-maxwell-mike-zarrillo" target="_blank"><img src="./dist/img/content/tile-zarrillo.png"></a>
+	    			<a href="http://info.maxwellhealth.com/blog/welcome-ferit" target="_blank"><img src="./dist/img/content/tile-ferit.png"></a>
+	    		</div>
+	    	</div>
+	    </div>
+	    <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>We Tracked Our Diversity</h2>
+	    		<p>A few highlights:</p>
+	    		<ul>
+	    			<li><span class="bold">57%</span> women vs. men in company.</li>
+	    			<li><span class="bold">44% </span>of managers with their own teams are women.</li>
+	    			<li><span class="bold">34%</span> of our Product Development team are women (other tech companies are<span class="bold"></span> at 10-17%).</li>
+	    			<li>
+	    				<ul>
+	    					<li><span class="bold">23%</span> of our engineers are women, while the total engineering workforce is at 14%.</li>
+	    					<li><span class="bold">4 out of 6</span> Product Management roles are held by women, including one Director.</li>
+	    				</ul>
+	    			</li>
+	    			<li>We started a <span class="bold">mentorship program</span> to foster more diversity and make sure we're promoting from within whenever possible.</li>
+	    		</ul>
+	    	</div>
+	    </div>
+	    <div class="section">
+	    	<div class="container">
+	    		<span>In 2015...</span>
+	    		<h2>We Nearly Tripled Our Team</h2>
+	    		<p>from <span class="bold">60</span> to <span class="bold">150+</span> Maxwellians</p>
+	    		<img class="mainImage" src="./dist/img/content/team.png">
+	    	</div>
+	    </div>
+	    <div class="section">
+	    	<div class="container">
+	    		<h2>We Got A New Home!</h2>
+	    		<ul>
+	    			<li>Our headquarters grew from 3,200 square feet on one floor to <span class="bold">14,000 square feet</span> on <span class="bold">two connected floors</span> in 101 Tremont Street today.</li>
+	    			<li>And we have offices all around the country:
+	    				<img class="mainImage map" src="./dist/img/content/us-map.svg">
+	    			</li>
+	    		</ul>
+	    	</div>
+	    </div>
+
+
+	    <!-- CHAP 6 -->
+	    <div class="section middle">
+	    	<div class="outro">
+	    		<h2>HERE’S TO AN EVEN BIGGER 2016</h2>
+	    		<p>We hope you’ve enjoyed learning a little more about us. Feel free to share this, and get in touch if you’re interested in being a part of the Maxwell story! </p>
+	    		<div class="nav-container2">
+				    <ul class="social-media-list">
+				        <li>
+				        	<!-- Twitter -->
+				            <a href="https://twitter.com/share" class="twitter-share-button"{count} data-text="Another incredible year for @maxwellhealth! Check out their stunning statistics in their Annual Report!" data-via="maxwellhealth" data-hashtags="MHendofyear2015" data-dnt="true">Tweet</a>
+				        </li>
+				        <li>
+				           <!-- Facebook -->
+				           <div id="fb-root"></div>
+				           <div class="fb-share-button" data-href="https://www.maxwellhealth.com/2015-report" data-layout="button"></div>
+				        </li>
+				        <li> 
+				        	<!-- Linkedin -->
+				            <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
+							<script type="IN/Share" data-url="https://www.maxwellhealth.com/2015-report"></script>
+				        </li>
+				    </ul>
+				    
+				</div>
+				<div class="buttonContainer">
+				    	<a href="https://www.maxwellhealth.com/aboutus" class="btn outro">About Maxwell</a>
+						<a href="https://www.maxwellhealth.com/careers" class="btn outro">We're Hiring!</a>
+					</div>
+				
+	    	</div>
+	    </div>
+	    
+	</div>
+
+	<div class="main main-position">
+		<?php echo file_get_contents("./dist/img/front/building1.svg"); ?>
+		<?php echo file_get_contents("./dist/img/front/building2.svg"); ?>
+		<?php echo file_get_contents("./dist/img/front/building3.svg"); ?>
+		<?php echo file_get_contents("./dist/img/front/building4.svg"); ?>
+
+
+		<?php echo file_get_contents("./dist/img/front/road.svg"); ?>
+		<!-- Vehicles -->
+		
+	</div>
+	<div class="vehicles road-position-start">
+		<?php echo file_get_contents("./dist/img/movers/car1.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/truck1.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/car2.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/truck2.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/car3.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/car4.svg"); ?>
+		<?php echo file_get_contents("./dist/img/movers/duckboat.svg"); ?>
+	</div>
+	
+	<!-- BACKGROUND BUILDINGS -->
+	
+	<div id="background1" class="bg-position">
+		<?php echo file_get_contents("./dist/img/background/bg-buildings1.svg"); ?>
+	</div>
+	<div id="background2" class="bg-position">
+		<?php echo file_get_contents("./dist/img/background/bg-buildings2.svg"); ?>
+	</div>
+
+	<!-- SCRIPTS -->
+	<script type="text/javascript" src="./dist/js/jquery-3-alpha.min.js"></script>
+	<script type="text/javascript" src="./dist/js/skrollr.min.js"></script>
+	<script type="text/javascript" src="./dist/js/jquery.fullpage.min.js"></script>
+	<script type="text/javascript" src="./dist/js/main.min.js"></script>
+
+</body>
+</html>
