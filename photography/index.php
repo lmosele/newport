6b8ef422cba639cfc5e5d0f354b7a404ca00ed34
Lucas Mosele <lucasmosele@gmail.com>
2016-06-24 14:43:14 -0400

    index order changes

--- photography/index.php
+++ photography/index.php
@@ -4,27 +4,27 @@
     <?php include 'module/_navbar.php' ?>
     <section class="portraitContent">
         <!-- picture -->
-        <a href="#olivia1"><img class="portrait" title="Olivia Poulin - Maggie Inc" id="olivia1" data-src="img/portrait/olivia-2.jpg"  src="img/src/loader.gif"></a>
-        <!-- picture -->
-        <a href="#funk5"><img class="portrait" title="Mike Beaudoin - Deep Funk" id="funk5" data-src="img/portrait/funk5.jpg"  src="img/src/loader.gif"></a>
-        <!-- picture -->
-        <a href="#kaileigh4"><img class="portrait" title="Kaileigh Bumpus - Portrait" id="kaileigh4" data-src="img/portrait/kaileigh-4.jpg"  src="img/src/loader.gif"></a>
-
-        <!-- picture -->
         <a href="#kristina6"><img class="portrait" title="Kristina Rugnetta - Miss Massachusetts Competitor" id="kristina6" data-src="img/portrait/kristina6.jpg"  src="img/src/loader.gif"></a>
         <!-- picture -->
         <a href="#melanie1"><img class="portrait" title="Melanie French - Portfolio" id="melanie1" data-src="img/portrait/melanie1.jpg"  src="img/src/loader.gif"></a>
         <!-- picture -->
-        <a href="#annette"><img class="portrait" title="Annette Chu" id="annette" data-src="img/portrait/annette-1.jpg"  src="img/src/loader.gif"></a>
+        <a href="#olivia1"><img class="portrait" title="Olivia Poulin - Maggie Inc" id="olivia1" data-src="img/portrait/olivia-2.jpg"  src="img/src/loader.gif"></a>
+        <!-- picture -->
+        <a href="#funk5"><img class="portrait" title="Mike Beaudoin - Deep Funk" id="funk5" data-src="img/portrait/funk5.jpg"  src="img/src/loader.gif"></a>
+        <!-- picture -->
+        <a href="#mar1"><img class="portrait" title="Mariely" id="mar1" data-src="img/portrait/mariely-1.jpg"  src="img/src/loader.gif"></a>
+        <!-- picture -->
+        <a href="#wedding1"><img id="wedding1" data-src="img/lifestyle/wedding-1.jpg" class="lifestyle"  src="img/src/loader.gif"></a>
         <!-- picture -->
         <a href="#glitch1"><img class="portrait" title="edIT - The Glitch Mob" id="glitch1" data-src="img/lifestyle/glitch1.jpg"  src="img/src/loader.gif"></a>
-<!-- picture -->
-        <a href="#mar1"><img class="portrait" title="Mariely" id="mar1" data-src="img/portrait/mariely-1.jpg"  src="img/src/loader.gif"></a>
+
 
         <!-- picture -->
         <a href="#kristina2"><img class="portrait" title="Kristina Rugnetta - Miss Massachusetts Competitor" id="kristina2" data-src="img/portrait/kristina2.jpg"  src="img/src/loader.gif"></a>
                 <!-- picture -->
-        <a href="#funk4"><img class="portrait" title="Christhiane Santos - Deep Funk" id="funk4" data-src="img/portrait/funk4.jpg"  src="img/src/loader.gif"></a>
+        <a href="#funk3"><img class="portrait" title="Mike Beaudoin - Deep Funk" id="funk3" data-src="img/portrait/funk3.jpg"  src="img/src/loader.gif"></a>
+                <!-- picture -->
+<!--         <a href="#funk4"><img class="portrait" title="Christhiane Santos - Deep Funk" id="funk4" data-src="img/portrait/funk4.jpg"  src="img/src/loader.gif"></a> -->
                 <!-- picture -->
         <a href="#mien2"><img class="portrait" title="Jessica Nylander - Mien Studios" id="mien2" data-src="img/portrait/mien2.jpg"  src="img/src/loader.gif"></a>
                 <!-- picture -->
@@ -46,7 +46,7 @@
         <!-- picture -->
         <a href="#glitch2"><img class="portrait" title="Boreta - Glitch Mob" id="glitch2" data-src="img/lifestyle/glitch2.jpg"  src="img/src/loader.gif"></a>
         <!-- picture -->
-        <a href="#kaileigh2"><img class="portrait" title="Kaileigh" id="kaileigh2" data-src="img/portrait/kaileigh2.jpg"  src="img/src/loader.gif"></a>
+<!--         <a href="#kaileigh2"><img class="portrait" title="Kaileigh" id="kaileigh2" data-src="img/portrait/kaileigh2.jpg"  src="img/src/loader.gif"></a> -->
 
         <!-- picture -->
 <!--         <a href="#kristina4"><img class="portrait" title="Kristina Rugnetta - Miss Massachusetts Competitor" id="kristina4" data-src="img/portrait/kristina4.jpg"  src="img/src/loader.gif"></a>
@@ -56,11 +56,11 @@
         <!-- picture -->
         <a href="#melanie3"><img class="portrait" title="Melanie French - Portfolio" id="melanie3" data-src="img/portrait/melanie3.jpg"  src="img/src/loader.gif"></a>
         <!-- picture -->
-        <a href="#funk3"><img class="portrait" title="Mike Beaudoin - Deep Funk" id="funk3" data-src="img/portrait/funk3.jpg"  src="img/src/loader.gif"></a>
+        <a href="#annette"><img class="portrait" title="Annette Chu" id="annette" data-src="img/portrait/annette-1.jpg"  src="img/src/loader.gif"></a>
         <!-- picture -->
         <!-- <a href="#autumn1"><img class="portrait" title="Autumn Goodwin - Portfolio" id="autumn1" data-src="img/portrait/autumn1.jpg"  src="img/src/loader.gif"></a> -->
         <!-- picture -->
-        <!-- <a href="#self"><img class="portrait" title="Self Portrait" id="self" data-src="img/portrait/self.jpg"  src="img/src/loader.gif"></a> -->
+        <a href="#self"><img class="portrait" title="Self Portrait" id="self" data-src="img/portrait/self.jpg"  src="img/src/loader.gif"></a>
         <!-- picture -->
         <!-- <a href="#melanie2"><img class="portrait" title="Melanie French - Portfolio" id="melanie2" data-src="img/portrait/melanie2.jpg"  src="img/src/loader.gif"></a> -->
         <!-- picture -->
