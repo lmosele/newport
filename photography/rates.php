1cbb6ebfc277c5fd6d68de36135bd36d08eb0184
Lucas Mosele <lucasmosele@gmail.com>
2016-12-05 22:33:19 -0500

    update rates

--- photography/rates.php
+++ photography/rates.php
@@ -13,37 +13,37 @@
         <p>If you have any questions or comments on rates please contact me via email at lucasmosele@gmail.com or select a package to inquire about.</p>
         
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Basic Headshot Package">
-          <h3>Basic Headshot Package <span class="price" title="price">$60</span></h3>
+          <h3>Basic Headshot Package <span class="price" title="price">$100</span></h3>
           <p>Basic headshots include 5 edited headshots of one (1) person for casting calls, social media, dating websites or other informal requirements.</p>
         </a>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Portrait Package">
-          <h3>Portrait Package <span class="price" title="price">$100 - $150</span></h3>
+          <h3>Portrait Package <span class="price" title="price">$200</span></h3>
           <p>The portrait package includes at least 10 edited photos on an outdoors shoot. This is the standard package for people who need full-body shots for acting, modeling, music, or senior portraits. </p>
         </a>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Agency Package">
-          <h3>Agency Package <span class="price" title="price">$500 - $675</span></h3>
-          <p>The Agency package includes a themed portrait-set of edited photos as well as hair and make-up (add $100) and studio cost (add +75) </p>
+          <h3>Agency Package <span class="price" title="price">$500</span></h3>
+          <p>The Agency package includes a themed portrait-set of edited photos as well as hair and make-up (add $100) or studio cost (add $75) </p>
         </a>
 
         <hr/>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Family Package">
-          <h3>Family Package<span class="price" title="price">$200 - $350</span></h3>
-          <p>Due to popular demand I am now offering family portraits starting at $200. 20 pictures will be provided and a $25 fee per person after the fourth family member will be added. Headshots of individual family members can also be provided on request.</p>
+          <h3>Family Package<span class="price" title="price">$400</span></h3>
+          <p>Due to popular demand I am now offering family portraits starting at $400. 20 pictures will be provided and a $25 fee per person after the fifth family member will be added. Headshots of individual family members can also be provided on request.</p>
         </a>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Pet Portrait">
           <h3>Pet Portrait<span class="price" title="price">$50 - $100</span></h3>
-          <p>Perfect for those looking to preserve memories of their favorite family members, pet portraits are now available, travel costs not included. Prints of all sizes can be provided for a fee.</p>
+          <p>Perfect for those looking to preserve memories of their favorite family members, pet portraits are now available, travel costs not included.</p>
         </a>
 
         <hr/>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Engagement Package">
-          <h3>Engagement Package <span class="price" title="price">$150 - $200</span></h3>
-          <p>The basic engagement shoot includes a shoot on location with two (2) people, travel costs based on location</p>
+          <h3>Engagement Package <span class="price" title="price">$300</span></h3>
+          <p>The basic engagement shoot includes a shoot on a location of your choice with two (2) people.</p>
         </a>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Wedding Dress Package">
@@ -53,13 +53,13 @@
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Wedding Package">
           <h3>Wedding Package<span class="price" title="price">$1000 - $1500</span></h3>
-          <p>Perfect for small ceremonies, the wedding package includes two (2) photographers (me and an assistant) to capture the ceremony &amp; reception (3 hours). Additional time past that (preparation or afterparties) will receive an additional charge of $100 per hour. </p>
+          <p>Perfect for small ceremonies, the wedding package includes two (2) photographers (me and an assistant) to capture the ceremony &amp; reception (4 hours). Additional time past that (preparation or afterparties) will receive an additional charge of $95 per hour. </p>
         </a>
 
         <hr/>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Party Package">
-          <h3>Party Package<span class="price" title="price">$50/hr</span></h3>
+          <h3>Party Package<span class="price" title="price">$70/hr</span></h3>
           <p>Having a party and need a photographer to record the memories? This package includes a hourly rate and aims to capture the best moments with your favorite people, whether it's a party with family or friends. Two hour minimum.</p>
         </a>
 
