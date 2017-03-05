9aa6874d4e0347c7ee92582f42c117658e076aa0
Lucas Mosele <lucasmosele@gmail.com>
2017-03-05 18:23:01 -0500

    changed photo rates

--- photography/rates.php
+++ photography/rates.php
@@ -8,35 +8,29 @@
         <h1>Rates</h1>
 
         <h2>Available in Boston, RI &amp; New Hampshire. Rates may vary per state/travel requirements.</h2>
-        <em>* Please note that edited photos imply acne cleanup, stray hairs, blemishes, etc. Retouching services are available and included on more advanced shoots.</em>
-
+ 
         <p>If you have any questions or comments on rates please contact me via email at lucasmosele@gmail.com or select a package to inquire about.</p>
         
-        <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Basic Headshot Package">
-          <h3>Basic Headshot Package <span class="price" title="price">$100</span></h3>
-          <p>Basic headshots include 5 edited headshots of one (1) person for casting calls, social media, dating websites or other informal requirements.</p>
-        </a>
-
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Portrait Package">
-          <h3>Portrait Package <span class="price" title="price">$200</span></h3>
-          <p>The portrait package includes at least 10 edited photos on an outdoors shoot. This is the standard package for people who need full-body shots for acting, modeling, music, or senior portraits. </p>
+          <h3>Portrait Package <span class="price" title="price">$300</span></h3>
+          <p>The portrait package includes at least 20 edited photos on an outdoors shoot. This is the standard package for people who need full-body shots for acting, modeling, music, or senior portraits. Indoors locations can be selected at extra cost. </p>
         </a>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Agency Package">
-          <h3>Agency Package <span class="price" title="price">$500</span></h3>
-          <p>The Agency package includes a themed portrait-set of edited photos as well as hair and make-up (add $100) or studio cost (add $75) </p>
+          <h3>Agency Package <span class="price" title="price">$500-$600</span></h3>
+          <p>The Agency package includes a themed portrait-set of edited photos as well as hair/makeup/styling by a third party (if applicable). </p>
         </a>
 
         <hr/>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Family Package">
           <h3>Family Package<span class="price" title="price">$400</span></h3>
-          <p>Due to popular demand I am now offering family portraits starting at $400. 20 pictures will be provided and a $25 fee per person after the fifth family member will be added. Headshots of individual family members can also be provided on request.</p>
+          <p>Due to popular demand I am now offering family portraits starting at $400. 20 pictures will be provided and a $25 fee per person after the sixth family member will be added.</p>
         </a>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Pet Portrait">
-          <h3>Pet Portrait<span class="price" title="price">$50 - $100</span></h3>
-          <p>Perfect for those looking to preserve memories of their favorite family members, pet portraits are now available, travel costs not included.</p>
+          <h3>Pet Portrait<span class="price" title="price">$100</span></h3>
+          <p>Perfect for those looking to preserve memories of their favorite family members, pet portraits are now available.</p>
         </a>
 
         <hr/>
@@ -47,7 +41,7 @@
         </a>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Wedding Dress Package">
-          <h3>Wedding Dress Package<span class="price" title="price">$500 - $675</span></h3>
+          <h3>Wedding Dress Package<span class="price" title="price">$600 - $675</span></h3>
           <p>For brides who want to model the special dress in an editorial style shoot. Similar to the Agency Package but on location with MUA/Hair if required.</p>
         </a>
 
@@ -60,7 +54,7 @@
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Party Package">
           <h3>Party Package<span class="price" title="price">$70/hr</span></h3>
-          <p>Having a party and need a photographer to record the memories? This package includes a hourly rate and aims to capture the best moments with your favorite people, whether it's a party with family or friends. Two hour minimum.</p>
+          <p>Having a party and need a photographer to record the memories? This package includes a hourly rate and aims to capture the best moments with your favorite people, whether it's a party with family or friends. Three hour minimum.</p>
         </a>
 
         <a class="rate" href="mailto:lucasmosele@gmail.com?subject=Concert Package">
