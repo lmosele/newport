baaa1c475fe9c57c8a1bfb74d4ce722870afc615
Lucas Mosele <lucasmosele@gmail.com>
2017-04-12 20:45:49 -0400

    Added client site

---
+++ staging/README.md
@@ -0,0 +1,11 @@
+# wp-site
+This repo is creating the template for move to wordpress:
+
+In order to run Front-End
+`git clone`
+`npm install`
+`npm run watch-css`
+
+Run PHP Server Locally
+`php -S localhost:9000`
+
---
+++ staging/src/modules/_menu.php
@@ -0,0 +1,32 @@
+<nav class="top-nav" id="topNav">
+	<h1 class="nav-logo">
+	  <a href="#home">
+	    <img src="./dist/img/logo-white.png" alt="Kriss Law Atlantic Closing & Escrow" />
+	  </a>
+	</h1>
+	<ul class="nav-link-list">
+		<li class="nav-item"><a role="link" class="nav-link" href="#">Our Services</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">About</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">News &amp; Events</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">Our Team</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">Careers</a></li>
+		<li class="nav-item"><a role="link" class="nav-button" href="#">Contact Us</a></li>
+	</ul>
+	
+</nav>
+
+
+<div class="toggle-wrapper">
+	<a id="mobile-nav-toggle" href="#"><span></span></a>
+</div>
+<div class="mobile-bg"></div>
+<div class="mobile-nav"> 
+	<ul class="nav-link-list-mobile">
+		<li class="nav-item"><a role="link" class="nav-link" href="#">Our Services</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">About</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">News &amp; Events</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">Our Team</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">Careers</a></li>
+		<li class="nav-item"><a role="link" class="nav-link" href="#">Contact Us</a></li>
+	</ul>
+</div>
